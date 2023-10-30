0cb00c83fa974dfb9f4d5796c2083eaa25d1d674
alebc96 <alebecerrac96@gmail.com>
2023-10-30 09:46:25 +0100

    adding column controller

--- src/controllers/column.controller.ts
+++ src/controllers/column.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { getConnection } from "../database/database";
+import { getConnection } from '../database/database';
 
 export const createColumn = async (req: Request, res: Response) => {
     const {column_name, board_id} = req.body
@@ -13,5 +13,56 @@ export const createColumn = async (req: Request, res: Response) => {
     } catch (error: any) {
         if(error.code === "ER_NO_REFERENCED_ROW_2") res.status(404).send({msg: "Board not found"})
         else res.status(500).send(error)
+    }
+}
+
+export const getAllColumnsByBoradId = async (req: Request, res: Response) => {
+    const { id } = req.query
+    const connection = await getConnection()
+    try {
+        const result = await connection.query("SELECT * FROM columns WHERE board_id = ?", [id])
+        if(result.length > 0) res.status(200).send(result)
+        else res.status(404).send({msg: "Columns not found for this baord"})
+    } catch (error: any) {
+        if(error.code === "ER_NO_REFERENCED_ROW_2") res.status(404).send({msg: "Board not found"})
+        res.status(500).send(error)
+    }
+}
+
+export const getColumnById = async (req: Request, res: Response) => {
+    const { id } = req.query
+    const connection = await getConnection()
+    try {
+        const result = await connection.query("SELECT * FROM columns WHERE column_id = ?", [id])
+        if(result.length > 0) res.status(200).send(result)
+        else res.status(404).send({msg: "Columns not found"})
+    } catch (error: any) {
+        if(error.code === "ER_NO_REFERENCED_ROW_2") res.status(404).send({msg: "Board not found"})
+        res.status(500).send(error)
+    }
+}
+
+export const editColumnById = async (req: Request, res: Response) => {
+    const { id } = req.query
+    const { column_name } = req.body
+    const connection = await getConnection()
+    try {
+        const result: any = await connection.query("UPDATE columns SET column_name = ? WHERE column_id = ?", [column_name,id])
+        if(result.affectedRows > 0) res.status(200).send(result)
+        else res.status(404).send({msg: "Column not found"})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export const deleteColumnById = async (req: Request, res: Response) => {
+    const { id } = req.query
+    const connection = await getConnection()
+    try {
+        const result: any = await connection.query("DELETE FROM columns WHERE column_id = ?", [id])
+        if(result.affectedRows === 1) res.status(200).send(result)
+        else res.status(404).send({msg: "Column not found"})
+    } catch (error) {
+        res.status(500).send(error)
     }
 }
--- src/routes/column.routes.ts
+++ src/routes/column.routes.ts
@@ -1,8 +1,18 @@
-import { Router } from "express"
-import { createColumn } from '../controllers/column.controller';
+import { Router } from "express";
+import {
+  createColumn,
+  deleteColumnById,
+  editColumnById,
+  getAllColumnsByBoradId,
+  getColumnById,
+} from "../controllers/column.controller";
 
-const router = Router()
+const router = Router();
 
-router.post("/columns", createColumn)
+router.post("/columns", createColumn);
+router.get("/boards/columns", getAllColumnsByBoradId);
+router.get("/columns/column", getColumnById);
+router.put("/columns/column", editColumnById);
+router.delete("/columns", deleteColumnById);
 
-export default router
+export default router;
